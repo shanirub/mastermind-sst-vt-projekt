310a82744f20350d21cbb20ee6e75d8242b60990
srub <shani.rubovitch@gmail.com>
2020-07-07 18:31:30 +0200

    analyse guess - full corrects is correct.

--- game.py
+++ game.py
@@ -8,14 +8,15 @@ class Board:
     def __init__(self):
         self.colors = generate_new_board()
 
-    def check_guess(self, colors):
-        if colors == self.colors:
+    def check_guess(self, guess):
+        if guess == self.colors:
             # correct guess, game ended
-            return True
+            print("OLL KORRECT")
         else:
             # incorrect guess, analyse guess
-            self.analyse_guess(colors)
-            return False
+            full_corrects, half_corrects = self.analyse_guess(guess)
+            print("Full Correct: " + str(full_corrects))
+            print("Half Correct: " + str(half_corrects))
 
     def print_board(self):
         board_str = ""
@@ -24,20 +25,37 @@ class Board:
 
         print(board_str)
 
-    def analyse_guess(self, colors):
-        pass
+    def analyse_guess(self, guess):
+        full_corrects = 0  # correct color + place
+        half_corrects = 0  # correct color wrong place (after full corrects removed)
+        copy_board = self.colors
+
+        for i in range(8):
+            if guess[i] == self.colors[i]:     # correct color + place counted and then removed
+                full_corrects += 1
+                guess[i] = ''
+                copy_board[i] = ''
+
+        for peg in guess:
+            if peg in copy_board:
+                half_corrects += 1
+                copy_board.remove(peg)
+
+        return full_corrects, half_corrects
 
 
 def generate_new_board():
-    # allowed_letters = "ABCDEFGH"
     # possible colors: light red, light green, light yellow, light blue
     allowed_colors = [fg.li_red, fg.li_green, fg.li_yellow, fg.li_blue]
-    # letters = choices(allowed_letters, k=8)     # choose 8 letters, can repeat
     colors = choices(allowed_colors, k=8)       # choose 8 colors, can repeat
 
     return colors
 
 
-b = Board()
-print(b)
-b.print_board()
+b1 = Board()
+b2 = Board()
+
+b1.print_board()
+b2.print_board()
+
+b1.check_guess(b2.colors)
