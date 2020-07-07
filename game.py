66a7b46b5b642c9e5208ccc426c996e4a22ba490
srub <shani.rubovitch@gmail.com>
2020-07-07 18:11:35 +0200

    need to guess only colors (no more letters)

--- game.py
+++ game.py
@@ -1,31 +1,41 @@
 from sty import fg, rs
 from random import choices
-RESET_FG_COLOR = "rs.fg"
+RESET_FG_COLOR = rs.fg  # represents resetting the foreground color
 
 
 class Board:
 
     def __init__(self):
-        self.letters, self.colors = generate_new_board()
+        self.colors = generate_new_board()
 
-    def check_guess(self, letters, colors):
-        if letters == self.letters and colors == self.colors:
+    def check_guess(self, colors):
+        if colors == self.colors:
+            # correct guess, game ended
             return True
         else:
+            # incorrect guess, analyse guess
+            self.analyse_guess(colors)
             return False
 
     def print_board(self):
+        board_str = ""
         for i in range(8):
-            print(self.colors[i] + self.letters[i] + RESET_FG_COLOR)
+            board_str += (self.colors[i] + 'O ' + RESET_FG_COLOR)
+
+        print(board_str)
+
+    def analyse_guess(self, colors):
+        pass
 
 
 def generate_new_board():
-    allowed_letters = "ABCDEFGH"
-    allowed_colors = ["fg.li_red", "fg.li_green", "fg.li_yellow", "fg.li_blue"]
-    letters = choices(allowed_letters, k=8)
-    colors = choices(allowed_colors, k=8)
+    # allowed_letters = "ABCDEFGH"
+    # possible colors: light red, light green, light yellow, light blue
+    allowed_colors = [fg.li_red, fg.li_green, fg.li_yellow, fg.li_blue]
+    # letters = choices(allowed_letters, k=8)     # choose 8 letters, can repeat
+    colors = choices(allowed_colors, k=8)       # choose 8 colors, can repeat
 
-    return letters, colors
+    return colors
 
 
 b = Board()
