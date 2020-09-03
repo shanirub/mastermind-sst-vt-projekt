f0cb5521b5af6e13bad06bc66b5cf4c148bea3b8
srub <shani.rubovitch@gmail.com>
2020-09-03 14:30:57 +0200

    before

--- mastermind/logic/game_logic_client.py
+++ mastermind/logic/game_logic_client.py
@@ -6,27 +6,25 @@ from mastermind.logic.game_logic_server import ServerReply, ClientRequest
 
 
 def should_exit(reply):
-    if reply.get('op').name == ServerReply.GAME_FULL.name:
-        logging.error("game full. exiting")
+    if reply.get('op') == ServerReply.GAME_FULL:
+        logging.error("Game full. exiting")
         return True
-    elif reply.get('op').name == ServerReply.PLAYER_ALREADY_EXISTS.name:
-        logging.error("player already exists. exiting")
+    elif reply.get('op') == ServerReply.PLAYER_ALREADY_EXISTS:
+        logging.error("Player already exists. exiting")
         return True
 
     return False
 
 
 def get_op_new_request(reply):
-    if reply.get('op') is ServerReply.NOT_YOUR_TURN or reply.get('op') is ServerReply.GAME_STARTED_WAIT_FOR_TURN \
-            or reply.get('op') is ServerReply.WAITING_FOR_SECOND_PLAYER:
-        logging.warning("not your turn")
+    if reply.get('op') is ServerReply.NOT_YOUR_TURN or reply.get('op') is ServerReply.GAME_STARTED_WAIT_FOR_TURN or reply.get('op') is ServerReply.WAITING_FOR_SECOND_PLAYER:
+        logging.warning("Not your turn")
         return ClientRequest.CHECK_STATE
-    elif reply.get('op') is ServerReply.STATE_WAITING_FOR_GUESS or reply.get(
-            'op') is ServerReply.GAME_STARTED_YOUR_TURN:
-        logging.info("your turn to guess")
+    elif reply.get('op') is ServerReply.STATE_WAITING_FOR_GUESS or reply.get('op') is ServerReply.GAME_STARTED_YOUR_TURN:
+        logging.info("Your turn to guess")
         return ClientRequest.SEND_GUESS
     elif reply.get('op') is ServerReply.GUESS_RESULT:
-        logging.info("got result for the guess")
+        logging.info("Got result for the guess")
         return ClientRequest.SEND_GUESS
     elif reply.get('op') is ServerReply.GAME_OVER:
         # game lost
